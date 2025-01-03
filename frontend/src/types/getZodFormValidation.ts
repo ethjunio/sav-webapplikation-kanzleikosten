1ccaca27b52cfad7528aa14a01eefb8804ce06a5
Fabian Imhof <fabian.imhof@clavisit.com>
2025-01-03 09:56:38 +0100

    translate error messages

--- frontend/src/types/getZodFormValidation.ts
+++ frontend/src/types/getZodFormValidation.ts
@@ -1,81 +1,84 @@
+import useI18n from '@/translations/i18n';
 import { z } from 'zod';
 
+const translate = useI18n();
+
 export const zodFormValidationLocation = z.object({
-  locationType: z.string().min(1, 'errorMessages.locationTypeRequired'), // Error message from content.json
+  locationType: z.string().min(1, translate('errorMessages.locationTypeRequired')), // Error message from content.json
   locationNumber: z
     .string()
-    .min(1, 'errorMessages.locationNumberRequired') // Ensure the input is provided
+    .min(1, translate('errorMessages.locationNumberRequired')) // Ensure the input is provided
     .transform((val) => parseFloat(val)) // Convert the string to a number
     .refine((val) => !isNaN(val), {
-      message: 'errorMessages.locationNumberInvalid',
+      message: translate('errorMessages.locationNumberInvalid'),
     }) // Ensure it's a valid number
     .refine((val) => val > 0, {
-      message: 'errorMessages.locationNumberGreaterThanZero',
+      message: translate('errorMessages.locationNumberGreaterThanZero'),
     }) // Ensure it's positive
     .refine((val) => Number.isInteger(val), {
-      message: 'errorMessages.locationNumberInteger',
+      message: translate('errorMessages.locationNumberInteger'),
     }), // Ensure it's a full integer
 });
 
 export const zodFormValidationProcess = z.object({
   processLeadingPersonnel: z
     .string()
-    .min(1, 'errorMessages.processPersonnelPercentageRequired') // Ensure input is provided
+    .min(1, translate('errorMessages.processPersonnelPercentageRequired')) // Ensure input is provided
     .transform((val) => parseFloat(val)) // Convert the string to a number
     .refine((val) => !isNaN(val), {
-      message: 'errorMessages.processPersonnelPercentageInvalid',
+      message: translate('errorMessages.processPersonnelPercentageInvalid'),
     }) // Ensure it's a valid number
     .refine((val) => val >= 0 && val <= 100, {
-      message: 'errorMessages.processPersonnelPercentageRange',
+      message: translate('errorMessages.processPersonnelPercentageRange'),
     }), // Ensure percentage is between 0 and 100
-  serviceType: z.string().min(1, 'errorMessages.serviceTypeRequired'), // Ensure an option is selected
+  serviceType: z.string().min(1, translate('errorMessages.serviceTypeRequired')), // Ensure an option is selected
   employeesCount: z
     .string()
-    .min(1, 'errorMessages.employeeCountRequired') // Ensure input is provided
+    .min(1, translate('errorMessages.employeeCountRequired')) // Ensure input is provided
     .transform((val) => parseFloat(val)) // Convert the string to a number
     .refine((val) => !isNaN(val), {
-      message: 'errorMessages.employeeCountInvalid',
+      message: translate('errorMessages.employeeCountInvalid'),
     }) // Ensure it's a valid number
     .refine((val) => val > 0, {
-      message: 'errorMessages.employeeCountGreaterThanZero',
+      message: translate('errorMessages.employeeCountGreaterThanZero'),
     }) // Ensure it's positive
     .refine((val) => Number.isInteger(val), {
-      message: 'errorMessages.employeeCountInteger',
+      message: translate('errorMessages.employeeCountInteger'),
     }), // Ensure it's a full integer
   partnersCount: z
     .string()
-    .min(1, 'errorMessages.partnerCountRequired') // Ensure input is provided
+    .min(1, translate('errorMessages.partnerCountRequired')) // Ensure input is provided
     .transform((val) => parseFloat(val)) // Convert the string to a number
     .refine((val) => !isNaN(val), {
-      message: 'errorMessages.partnerCountInvalid',
+      message: translate('errorMessages.partnerCountInvalid'),
     }) // Ensure it's a valid number
     .refine((val) => val > 0, {
-      message: 'errorMessages.partnerCountGreaterThanZero',
+      message: translate('errorMessages.partnerCountGreaterThanZero'),
     }) // Ensure it's positive
     .refine((val) => Number.isInteger(val), {
-      message: 'errorMessages.partnerCountInteger',
+      message: translate('errorMessages.partnerCountInteger'),
     }), // Ensure it's a full integer
 });
 
 export const zodFormValidationFinances = z.object({
   revenuePerYear: z
     .string()
-    .min(1, 'errorMessages.revenuePerYearRequired') // Ensure input is provided
+    .min(1, translate('errorMessages.revenuePerYearRequired')) // Ensure input is provided
     .transform((val) => parseFloat(val)) // Convert the string to a number
     .refine((val) => !isNaN(val), {
-      message: 'errorMessages.revenuePerYearInvalid',
+      message: translate('errorMessages.revenuePerYearInvalid'),
     }) // Ensure it's a valid number
     .refine((val) => val > 0, {
-      message: 'errorMessages.revenuePerYearGreaterThanZero',
+      message: translate('errorMessages.revenuePerYearGreaterThanZero'),
     }), // Ensure it's positive
   operatingCostsPerYear: z
     .string()
-    .min(1, 'errorMessages.operatingCostsPerYearRequired') // Ensure input is provided
+    .min(1, translate('errorMessages.operatingCostsPerYearRequired')) // Ensure input is provided
     .transform((val) => parseFloat(val)) // Convert the string to a number
     .refine((val) => !isNaN(val), {
-      message: 'errorMessages.operatingCostsPerYearInvalid',
+      message: translate('errorMessages.operatingCostsPerYearInvalid'),
     }) // Ensure it's a valid number
     .refine((val) => val > 0, {
-      message: 'errorMessages.operatingCostsPerYearGreaterThanZero',
+      message: translate('errorMessages.operatingCostsPerYearGreaterThanZero'),
     }), // Ensure it's positive
 });
